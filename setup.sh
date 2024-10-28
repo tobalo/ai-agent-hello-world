818099d4f486b9994488003f1f0b5bbc2d06afbd
tobalo <cristobaltorres@cristobals-mbp.lan>
2024-10-28 08:46:53 -0500

    dev: add context

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 # Create a virtual environment
 python3 -m venv ./aienv
-
 # Activate the virtual environment
 source ./aienv/bin/activate
-
 # Install dependencies
 pip3 install -r requirements.txt
 
